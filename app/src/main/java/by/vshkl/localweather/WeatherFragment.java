4edf8854325c786a19a3e6c11861b01ad98b9d9d
Pavel Vashkel <jgugn1@gmail.com>
2015-11-01 18:19:39 +0300

    UI Blueprint

---
+++ app/src/main/java/by/vshkl/localweather/WeatherFragment.java
@@ -0,0 +1,25 @@
+package by.vshkl.localweather;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+
+    public WeatherFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+}
